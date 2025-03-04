4499f5f77b7f3e0a8a680d3bf2567d617aa4ad2a
harvey <alstjr971@naver.com>
2025-03-04 22:33:16 +0900

    test(user): user service test
    유저 서비스 테스트

---
+++ src/test/java/mayfifth99/twitter/user/repository/FakeUserRelationRepository.java
@@ -0,0 +1,26 @@
+package mayfifth99.twitter.user.repository;
+
+import java.util.HashSet;
+import java.util.Set;
+import mayfifth99.twitter.user.application.interfaces.UserRelationRepository;
+import mayfifth99.twitter.user.domain.User;
+
+public class FakeUserRelationRepository implements UserRelationRepository {
+    private final Set<Relation> store = new HashSet<>();
+
+    @Override
+    public boolean isAlreadyFollow(User user, User targetUser) {
+        return store.contains(new Relation(user.getId(), targetUser.getId()));
+    }
+
+    @Override
+    public void save(User user, User targetUser) {
+        store.add(new Relation(user.getId(), targetUser.getId()));
+    }
+
+    @Override
+    public void delete(User user, User targetUser) {
+        store.remove(new Relation(user.getId(), targetUser.getId()));
+    }
+}
+record Relation(Long userId, Long targetUserId){}
---
+++ src/test/java/mayfifth99/twitter/user/repository/FakeUserRepository.java
@@ -0,0 +1,27 @@
+package mayfifth99.twitter.user.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import mayfifth99.twitter.user.application.interfaces.UserRepository;
+import mayfifth99.twitter.user.domain.User;
+
+public class FakeUserRepository implements UserRepository {
+
+    private final Map<Long, User> store = new HashMap<>();
+    @Override
+    public User save(User user) {
+        if(user.getId() != null){
+            store.put(user.getId(), user);
+        }
+        Long id = store.size() + 1L;
+        User newUser = new User(id, user.getUserInfo());
+        store.put(id, newUser);
+        return newUser;
+    }
+
+    @Override
+    public Optional<User> findById(Long userId) {
+        return Optional.ofNullable(store.get(userId));
+    }
+}
