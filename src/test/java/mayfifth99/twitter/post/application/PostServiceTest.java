244d62df8cbaf424110cdd4ca82bd087a765a1ef
harvey <alstjr971@naver.com>
2025-03-05 18:57:55 +0900

    test(post-comment): Post, Comment 서비스 테스트 구현

---
+++ src/test/java/mayfifth99/twitter/post/application/PostServiceTest.java
@@ -0,0 +1,117 @@
+package mayfifth99.twitter.post.application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import mayfifth99.twitter.post.application.dto.LikeRequestDto;
+import mayfifth99.twitter.post.application.dto.UpdatePostRequestDto;
+import mayfifth99.twitter.post.domain.Post;
+import mayfifth99.twitter.post.domain.content.PostPublicationState;
+import org.junit.jupiter.api.Test;
+
+class PostServiceTest extends PostApplicationTestTemplate {
+    @Test
+    void givenPostRequestDto_whenCreate_thenReturnPost() throws Exception {
+        //when
+        Post savedPost = postService.createPost(postDto);
+
+        //then
+
+        Post post = postService.getPost(savedPost.getId());
+        assertEquals(post, savedPost);
+    }
+
+    @Test
+    void givenCreatePost_whenUpdate_thenReturnUpdatedPost() throws Exception {
+        //when
+        Post savedPost = postService.createPost(postDto);
+
+        //then
+        UpdatePostRequestDto updatePostRequestDto = new UpdatePostRequestDto(savedPost.getId(),
+                user.getId(), "this is updated", PostPublicationState.PRIVATE);
+        postService.updatePost(updatePostRequestDto);
+
+        //then
+        Post post = postService.getPost(savedPost.getId());
+        assertEquals(post.getContent().getContent(), "this is updated");
+        assertEquals(post.getState(), PostPublicationState.PRIVATE);
+    }
+
+    @Test
+    void givenCreatedPost_whenLiked_thenReturnPostWithLike() throws Exception {
+        //given
+        Post post = postService.createPost(postDto);
+
+        //when
+        LikeRequestDto likeDto = new LikeRequestDto(post.getId(), otherUser.getId());
+        postService.likePost(likeDto);
+
+        //then
+        Post findPost = postService.getPost(post.getId());
+        assertEquals(findPost.getLikeCount(), 1);
+    }
+
+    @Test
+    void givenCreatedPost_whenLikeTwice_thenReturnLikeCountOne() throws Exception {
+        //given
+        Post post = postService.createPost(postDto);
+
+        //when
+        LikeRequestDto likeDto = new LikeRequestDto(post.getId(), otherUser.getId());
+        postService.likePost(likeDto);
+        postService.likePost(likeDto);
+
+        //then
+        Post findPost = postService.getPost(post.getId());
+        assertEquals(findPost.getLikeCount(), 1);
+    }
+
+    @Test
+    void givenLikedPost_whenUnliked_thenReturnPostWithLike() throws Exception {
+        //given
+        Post post = postService.createPost(postDto);
+        LikeRequestDto likeDto = new LikeRequestDto(post.getId(), otherUser.getId());
+        postService.likePost(likeDto);
+
+        //when
+        postService.unlikePost(likeDto);
+
+        //then
+        Post findPost = postService.getPost(post.getId());
+        assertEquals(findPost.getLikeCount(), 0);
+    }
+
+
+    @Test
+    void givenCreatedPost_whenUnliked_thenReturnPostWithoutLike() {
+        // given
+        Post savedPost = postService.createPost(postDto);
+
+        // when
+        LikeRequestDto likeRequestDto = new LikeRequestDto(savedPost.getId(),otherUser.getId());
+        postService.likePost(likeRequestDto);
+        postService.unlikePost(likeRequestDto);
+
+        // then
+        assertEquals(0, savedPost.getLikeCount());
+    }
+
+    @Test
+    void givenCreatedPostWhenUnlikedTwiceThenReturnPostWithoutLike() {
+        // given
+        Post savedPost = postService.createPost(postDto);
+
+        // when
+        LikeRequestDto likeRequestDto = new LikeRequestDto(savedPost.getId(),otherUser.getId());
+        postService.likePost(likeRequestDto);
+        postService.unlikePost(likeRequestDto);
+        postService.unlikePost(likeRequestDto);
+
+        // then
+        assertEquals(0, savedPost.getLikeCount());
+    }
+
+
+
+
+}
+
