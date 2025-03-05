bab2f5ab1244cbaa9ad7e31b2b19180f6c668a0d
Harvey <149806254+MAYFIFTH99@users.noreply.github.com>
2025-03-05 11:18:39 +0900

    test(post-comment): Post, Comment Domain Test (#5)

---
+++ src/test/java/mayfifth99/twitter/post/common/DateTimeInfoTest.java
@@ -0,0 +1,26 @@
+package mayfifth99.twitter.post.common;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.Test;
+
+class DateTimeInfoTest {
+
+
+    @Test
+    void givenCreated_whenUpdated_thenTimeAndStateArgsUpdated() throws Exception {
+        //given
+        DateTimeInfo dateTimeInfo = new DateTimeInfo();
+        LocalDateTime beforeUpdate = dateTimeInfo.getDateTime();
+
+        Thread.sleep(1);
+        //when
+        dateTimeInfo.update();
+
+        //then
+        assertTrue(dateTimeInfo.isEdited());
+        assertNotEquals(beforeUpdate, dateTimeInfo.getDateTime());
+    }
+}
