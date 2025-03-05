b25560788c6b095cc521b6145283c2d4b8a23f1c
harvey <alstjr971@naver.com>
2025-03-05 19:09:04 +0900

    chore: validation 의존성 추가, 헬스체크 컨트롤러 추가

---
+++ src/main/java/mayfifth99/twitter/common/ui/HealthController.java
@@ -0,0 +1,13 @@
+package mayfifth99.twitter.common.ui;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "OK";
+    }
+}
