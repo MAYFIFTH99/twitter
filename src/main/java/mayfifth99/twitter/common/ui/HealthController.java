e09a13035c4b5b9e878afb85173340fba2d95281
harvey <alstjr971@naver.com>
2025-03-06 11:38:28 +0900

    docs: 공통 Response 생성, Swagger 의존성 추가, HealthController 추가

--- src/main/java/mayfifth99/twitter/common/ui/HealthController.java
+++ src/main/java/mayfifth99/twitter/common/ui/HealthController.java
@@ -1,9 +1,11 @@
 package mayfifth99.twitter.common.ui;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Health Check")
 public class HealthController {
 
     @GetMapping("/")
---
+++ src/main/java/mayfifth99/twitter/user/ui/controller/UserControllerSpec.java
@@ -0,0 +1,35 @@
+package mayfifth99.twitter.user.ui.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import mayfifth99.twitter.common.ui.Response;
+import mayfifth99.twitter.user.application.dto.CreateUserRequestDto;
+
+@Tag(name = "유저 API")
+public interface UserControllerSpec {
+
+    @Operation(summary = "유저 생성",
+            requestBody = @RequestBody(
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    examples = {
+                                            @ExampleObject(name = "유저 생성 요청", value = CREATE_USER_REQUEST)
+
+                                    }
+                            )
+                    }
+            )
+    )
+    Response<Long> createUser(CreateUserRequestDto dto);
+
+    String CREATE_USER_REQUEST = """
+            {
+              "name": "userA",
+              "profileImageUrl": "example.com"
+            }
+            """;
+}
