349333d490f72d8eb9ab3bd133871587eb2ac7a4
harvey <alstjr971@naver.com>
2025-03-08 17:53:32 +0900

    test: 매 테스트마다 테이블 초기화 SQL 추가

--- src/test/java/mayfifth99/twitter/acceptance/utils/DataLoader.java
+++ src/test/java/mayfifth99/twitter/acceptance/utils/DataLoader.java
@@ -3,12 +3,17 @@ package mayfifth99.twitter.acceptance.utils;
 import static mayfifth99.twitter.acceptance.steps.UserAcceptanceSteps.createUser;
 import static mayfifth99.twitter.acceptance.steps.UserAcceptanceSteps.followUser;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import mayfifth99.twitter.user.application.dto.CreateUserRequestDto;
 import mayfifth99.twitter.user.application.dto.FollowUserRequestDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DataLoader {
+
+    @PersistenceContext
+    private EntityManager em;
 
     public void loadData(){
         CreateUserRequestDto dto = new CreateUserRequestDto("test user", "");
@@ -19,4 +24,25 @@ public class DataLoader {
         followUser(new FollowUserRequestDto(1L, 2L));
         followUser(new FollowUserRequestDto(1L, 3L));
     }
+
+    public String getEmailToken(String email){
+        return em.createNativeQuery("SELECT token FROM community_email_verification WHERE email = ?", String.class)
+                .setParameter(1, email)
+                .getSingleResult()
+                .toString();
+    }
+
+    public boolean isEmailVerified(String email){
+        return em.createQuery("SELECT e.isVerified FROM EmailVerificationEntity e WHERE email = :email", Boolean.class)
+                .setParameter("email", email)
+                .getSingleResult();
+    }
+
+    public Long getUserId(String email){
+        return (Long) em.createNativeQuery("SELECT user_id FROM community_user_auth WHERE email = ?", Long.class)
+                .setParameter(1, email)
+                .getSingleResult();
+    }
+
+
 }
