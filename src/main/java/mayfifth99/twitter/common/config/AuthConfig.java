c35b5d549d3689a2bdf7d141413b63862dc208fd
harvey <alstjr971@naver.com>
2025-03-09 23:36:49 +0900

    refact(auth): AuthConfig에서 TokenProvider 의존 관계 주입을 생성자에서 @RequiredArgs 어노테이션으로 변경

--- src/main/java/mayfifth99/twitter/common/config/AuthConfig.java
+++ src/main/java/mayfifth99/twitter/common/config/AuthConfig.java
@@ -1,20 +1,17 @@
 package mayfifth99.twitter.common.config;
 
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import mayfifth99.twitter.auth.domain.TokenProvider;
 import mayfifth99.twitter.common.principal.AuthPrincipalArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class AuthConfig implements WebMvcConfigurer {
 
     private final TokenProvider tokenProvider;
-
-
-    public AuthConfig(TokenProvider tokenProvider) {
-        this.tokenProvider = tokenProvider;
-    }
 
     @Override
     public void addArgumentResolvers(List argumentResolvers) {
