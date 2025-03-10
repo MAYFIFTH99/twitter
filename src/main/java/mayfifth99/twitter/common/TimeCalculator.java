8d583862c63caeb5d32604825fd9cac1bceccaaf
harvey <alstjr971@naver.com>
2025-03-10 11:28:41 +0900

    docs: bootstraps 로 대시보드 UI 구현, 타임리프 의존성 추가, DB MySQL로 변경

---
+++ src/main/java/mayfifth99/twitter/common/TimeCalculator.java
@@ -0,0 +1,15 @@
+package mayfifth99.twitter.common;
+
+import java.time.LocalDate;
+
+public class TimeCalculator {
+
+    private TimeCalculator() {
+    }
+
+    public static LocalDate getDateDaysAgo(int daysAgo){
+        LocalDate currDate = LocalDate.now();
+        return currDate.minusDays(daysAgo);
+    }
+
+}
