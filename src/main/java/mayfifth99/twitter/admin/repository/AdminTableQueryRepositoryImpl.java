58351588d030afe98d21aaa2fe13b30a42571fa1
harvey <alstjr971@naver.com>
2025-03-11 00:20:44 +0900

    perf(admin): 커버링 인덱스를 이용하여 전체 사용자 조회 시간 3->1초 로 성능 개선

--- src/main/java/mayfifth99/twitter/admin/repository/AdminTableQueryRepositoryImpl.java
+++ src/main/java/mayfifth99/twitter/admin/repository/AdminTableQueryRepositoryImpl.java
@@ -21,12 +21,11 @@ public class AdminTableQueryRepositoryImpl implements AdminTableQueryRepository 
     QUserAuthEntity userAuthEntity = QUserAuthEntity.userAuthEntity;
     QUserEntity userEntity = QUserEntity.userEntity;
 
+    // 50만개의 사용자 데이터가 있을 때, 3초 -> 1초로 성능 개선
     @Override // 이름으로 사용자 목록 조회
     public GetTableListResponse<GetUserTableResponseDto> getUserTableData(
             GetUserTableRequestDto dto) {
 
-
-        //TODO total 조회 쿼리 성능 개선 필요
         int total = queryFactory
                 .select(userEntity.id)
                 .from(userEntity)
@@ -34,27 +33,64 @@ public class AdminTableQueryRepositoryImpl implements AdminTableQueryRepository 
                 .fetch()
                 .size();
 
-        List<GetUserTableResponseDto> result = queryFactory
-                .select(Projections.fields(
-                        GetUserTableResponseDto.class,
-                        userEntity.id.as("id"),
-                        userEntity.name.as("name"),
-                        userAuthEntity.email.as("email"),
-                        userAuthEntity.userRole.as("role"),
-                        userEntity.regDt.as("createdAt"),
-                        userEntity.updDt.as("updatedAt"),
-                        userAuthEntity.lastLoginDt.as("lastLoginAt")
-                ))
+        // 커버링 인덱스 적용
+        List<Long> ids = queryFactory
+                .select(userEntity.id)
                 .from(userEntity)
-                .join(userAuthEntity)
-                .on(userEntity.id.eq(userAuthEntity.userId))
                 .where(likeName(dto.getName()))
                 .orderBy(userEntity.id.desc())
                 .offset(dto.getOffset())
                 .limit(dto.getLimit())
                 .fetch();
 
-        return new GetTableListResponse<>(total,result);
+        // 커버링 인덱스를 이용해 찾은 ids 들의 정보만 반환하면 성능 최적화 가능
+        List<GetUserTableResponseDto> result = queryFactory
+                .select(
+                        Projections.fields(
+                                GetUserTableResponseDto.class,
+                                userEntity.id.as("id"),
+                                userEntity.name.as("name"),
+                                userAuthEntity.email.as("email"),
+                                userAuthEntity.userRole.as("role"),
+                                userEntity.regDt.as("createdAt"),
+                                userEntity.updDt.as("updatedAt"),
+                                userAuthEntity.lastLoginDt.as("lastLoginAt")
+                        ))
+                                .from(userEntity)
+                                .join(userAuthEntity)
+                                .on(userEntity.id.eq(userAuthEntity.userId))
+                                .where(userEntity.id.in(ids)
+                ).orderBy(userEntity.id.desc())
+                .fetch();
+
+        return new GetTableListResponse<>(total, result);
+
+        //TODO total 조회 쿼리 성능 개선 필요
+
+        //커버링 인덱스 적용을 위해 해당 쿼리 폐기
+
+
+//        List<GetUserTableResponseDto> result = queryFactory
+//                .select(Projections.fields(
+//                        GetUserTableResponseDto.class,
+//                        userEntity.id.as("id"),
+//                        userEntity.name.as("name"),
+//                        userAuthEntity.email.as("email"),
+//                        userAuthEntity.userRole.as("role"),
+//                        userEntity.regDt.as("createdAt"),
+//                        userEntity.updDt.as("updatedAt"),
+//                        userAuthEntity.lastLoginDt.as("lastLoginAt")
+//                ))
+//                .from(userEntity)
+//                .join(userAuthEntity)
+//                .on(userEntity.id.eq(userAuthEntity.userId))
+//                .where(likeName(dto.getName()))
+//                .orderBy(userEntity.id.desc())
+//                .offset(dto.getOffset())
+//                .limit(dto.getLimit())
+//                .fetch();
+//
+//        return new GetTableListResponse<>(total,result);
     }
 
     private BooleanExpression likeName(String name) {
