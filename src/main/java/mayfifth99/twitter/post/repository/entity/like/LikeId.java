394bde9193f082227b0514fd7bac79ade80ffc80
harvey <alstjr971@naver.com>
2025-03-21 16:46:33 +0900

    refactor(like): LikeTarget Enum 타입이 DB에 ORDINAL 숫자가 아닌 STRING으로 저장되도록 변경

--- src/main/java/mayfifth99/twitter/post/repository/entity/like/LikeId.java
+++ src/main/java/mayfifth99/twitter/post/repository/entity/like/LikeId.java
@@ -1,6 +1,8 @@
 package mayfifth99.twitter.post.repository.entity.like;
 
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import java.io.Serializable;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -13,6 +15,7 @@ public class LikeId implements Serializable {
     private Long targetId;
     private Long userId;
 
+    @Enumerated(EnumType.STRING)
     private LikeTarget target;
 
     public LikeId(Long targetId, Long userId, LikeTarget target) {
