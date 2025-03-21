244001e26993cb50d8334dc68e5793057b9e3f53
harvey <alstjr971@naver.com>
2025-03-21 16:31:10 +0900

    chore(ui): UPDATE feedControllerSpec

--- src/main/java/mayfifth99/twitter/post/ui/controller/swagger/FeedControllerSpec.java
+++ src/main/java/mayfifth99/twitter/post/ui/controller/swagger/FeedControllerSpec.java
@@ -1,38 +1,20 @@
 package mayfifth99.twitter.post.ui.controller.swagger;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import mayfifth99.twitter.common.principal.UserPrincipal;
 import mayfifth99.twitter.common.ui.Response;
 import mayfifth99.twitter.post.ui.dto.GetPostContentResponseDto;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "피드 API")
 public interface FeedControllerSpec {
 
-    @Operation(summary = "게시물 조회",
-            requestBody = @RequestBody(
-                    content = {
-                            @Content(
-                                    mediaType = "application/json",
-                                    examples = {
-                                            @ExampleObject(name = "게시물 조회 요청", value = GET_POST_CONTENT)
-                                    }
-                            )
-                    }
-            )
-    )
-    Response<List<GetPostContentResponseDto>> getPostContent(UserPrincipal userPrincipal,
-            Long lastPostId);
-
-    String GET_POST_CONTENT = """
-            {
-              "userId": 1,
-              "userRole": "USER",
-              "lastPostId": 1
-            }
-            """;
+    @Operation(summary = "게시물 조회", description = "사용자가 팔로우하는 유저들의 게시물 조회.")
+    Response<List<GetPostContentResponseDto>> getPostContent(
+            @Parameter(hidden = true) UserPrincipal userPrincipal, // Auth 헤더로 처리됨
+            @Parameter(description = "마지막 게시물 ID (페이징 처리)") @RequestParam(name = "lastPostId", required = false) Long lastPostId
+    );
 }
